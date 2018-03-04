fadc0a458ddf16efb7cfbb85f1a6441a3990bf4c
LooseCannon711 <37031545+LooseCannon711@users.noreply.github.com>
2018-03-03 21:40:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.MzgwODYyOTMzOTg0MzQ2MTEy.DXzpaw.ZspuBkJeoDOApVxrYhsjQdhwz74);
+client.login(process.env.BOT_TOKEN);
